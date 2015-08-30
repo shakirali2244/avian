dceddba75b76c1d1d21bd16cfae6be21a30497cc
Shakir Ali <shakirali2244@yahoo.com>
2015-08-29 23:53:45 -0700

    takeoff, land, goto, beautification

--- app/scripts/lib/avian.js
+++ app/scripts/lib/avian.js
@@ -1,16 +1,16 @@
-var map;
+  var map;
 var markers = [];
 var drones = [];
+var socket;
 function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 49.277447, lng: -122.917120},
-    zoom: 14,
-    mapTypeId: google.maps.MapTypeId.HYBRID
+    zoom: 14
   });
 
 
-  var socket = io.connect('http://localhost:3000');
+  socket = io.connect('http://localhost:3000');
   socket.on('location', function(data){
     if (!map){
      map = new google.maps.Map(document.getElementById('map'), {
@@ -28,44 +28,70 @@ function initMap() {
   });
 
   map.addListener('click', function(event) {
+    setMapOnAll(null);
     addMarker(event.latLng);
   });
 
 }
 
-function addMarker(location) {
-    var marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-    markers.push(marker);
-
-    var contentString = '<button type="button"  onClick="deleteMarkers()" class="btn btn-default" aria-label="Left Align">
-        <span class="glyphicon glyphicon-trash" aria-hidden="true"> Clear Markers</span>
-      </button>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-    socket.emit('goto',{ lat : location.lat, lon: location.lng } );
-  }
-
 function addDrone(location) {
     var marker = new google.maps.Marker({
       position: location,
-      map: map
-    });
+      map: map,
+      icon: '/images/drone8.png'
+    })
     drones.push(marker);
   }
 
 function addMarker(location) {
     var marker = new google.maps.Marker({
       position: location,
-      map: map
+      map: map,
+      icon: '/images/goto8.png'
     });
-    markers.push(marker);
+        var contentString = 'Select Altitude <select class="form-control" id="altitude">'+
+    '<option value="0">0</option>'+
+    '<option value="10">10</option>'+
+    '<option value="30">30</option>'+
+    '</select>'+
+    '<br />'+
+    '<button type="button"  onClick="sendGoto()" class="btn btn-primary" aria-label="Left Align">'+
+    '<span class="glyphicon glyphicon-send" aria-hidden="true"> Goto</span>'+
+      '</button>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  infowindow.open(map,marker);
+    markers[0] = marker;
   }
+
+function sendGoto(){
+  var marker = markers[0];
+  var lat = marker.getPosition().lat()
+  var lon = marker.getPosition().lng()
+  var sel =  document.getElementById("altitude");
+  var alt = sel.options[sel.selectedIndex].value;
+  console.log('sending go to ' + lat + ', ' + lon + ', '+ alt);
+  socket.emit('goto',{ lat : lat, lon: lon, alt: alt} );
+}
+
+function sendLand(){
+  console.log('sending land with mode 1 i.e. emergency landing');
+  socket.emit('land',1);
+}
+
+function sendHome(){
+  console.log('sending land with mode 0 i.e. go home');
+  socket.emit('land',0);
+}
+
+function sendTakeoff(){
+  console.log('sending takeoff with alt 20');
+  socket.emit('takeoff',20);
+}
+
 
 function setMapOnAll(map) {
     for (var i = 0; i < markers.length; i++) {
