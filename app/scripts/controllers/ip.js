ee3c84f21c7c262a1efcc614a8b1ea8c9c8685ba
Akshay Budhiraja <akfastnos@gmail.com>
2015-09-09 20:57:07 -0700

    IP From Pi

---
+++ app/scripts/controllers/ip.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name avianApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the avianApp
+ */
+angular.module('avianApp')
+  .controller('ipCtrl', function ($scope) {
+
+  	$scope.ip = 'Waiting for emit...'
+
+  	var socket;
+  	socket = io.connect('http://localhost:3000');
+
+  	socket.on('ipFromServer', function(data){
+  		$scope.ip = data;
+  		socket.emit('ipAckFromClient', 'ack');
+  	});
+});
